256a5c27bf3819db9a3811921c2bf97087d03980
hardikkat24 <54679643+hardikkat24@users.noreply.github.com>
2020-02-09 12:05:53 +0530

    Update game-two.js

--- game-two.js
+++ game-two.js
@@ -4,7 +4,7 @@ var score;
 
 //To go up on clicking
 //accelerate function to be defined later
-document.getElementById("game-body").addEventListener ("click", accelerate(-0.3), false);
+//document.getElementById("game-body").addEventListener ("click", accelerate(-0.3), false);
 
 function startFlappyBird() {
     flappyBird= new component(30,30,"red",10,120);
@@ -63,5 +63,75 @@ function component(width, height, color, x, y, type) {
             this.gravitySpeed = 0;
         }
     }
+    this.crashWith = function(otherobj){
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+}
+
+function updateGameArea(){
+    var x, y, gap, minHeight, maxHeight, minGap, maxGap;
+    for(i = 0; i< obstacles.length; i = i + 1)
+    {
+        if(flappyBird.crashWith(obstacles[i]))
+            return;
+    }
+    
+    gameArea.clear();
+    gameArea.frameNo += 1;
+    if(gameArea.frameNo == 1 || everyinterval(150)){
+        x = gameArea.canvas.width;
+        y = gameArea.canvas.height;
+
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight - minHeight +1) + minHeight);
+
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap - minGap + 1) + minGap);
+
+        obstacles.push(new component(10, height, 'green', x, 0));
+        obstacles.push(new component(10, y - height - gap, 'green', x, height + gap));
+
+    }
+
+    for(i = 0; i < obstacles.length; i= i+1)
+    {
+        obstacles[i].x += -1;
+        obstacles[i].update()
+    }
+
+    score.text = gameArea.frameNo;
+    score.update();
+    flappyBird.newPos();
+    flappyBird.update();
+}
+
+function everyinterval(n)
+{
+    if((gameArea.frameNo/n)%1 == 0)
+        return true;
+    
+    return false;
+}
+function accelerate(n) {
+    if(!gameArea.interval) {
+        gameArea.interval=setInterval(updateGameArea,20);
+    }
+
+    flappyBird.gravity=n;
 
 }
